aee053d65b71c84aac88869309b4b31d18ec9676
Taehun Kang <agongee123@naver.com>
2020-04-28 10:14:10 +0900

    test v1.0

--- run.sh
+++ run.sh
@@ -1,13 +1,71 @@
-python simul.py --algo prema --batch 1 --num 4
-python simul.py --algo prema --batch 1 --num 4
-python simul.py --algo fcfs --batch 1 --num 4
-python simul.py --algo fcfs --batch 1 --num 4
-python simul.py --algo rrb --batch 1 --num 4
-python simul.py --algo rrb --batch 1 --num 4
-python simul.py --algo hpf --batch 1 --num 4
-python simul.py --algo hpf --batch 1 --num 4
-python simul.py --algo token --batch 1 --num 4
-python simul.py --algo token --batch 1 --num 4
-python simul.py --algo sjf --batch 1 --num 4
-python simul.py --algo sjf --batch 1 --num 4
+python simul.py --algo prema --batch 1 --num 16
+python simul.py --algo prema --batch 1 --num 16
+python simul.py --algo hpf --batch 1 --num 16
+python simul.py --algo hpf --batch 1 --num 16
+python simul.py --algo token --batch 1 --num 16
+python simul.py --algo token --batch 1 --num 16
+python simul.py --algo sjf --batch 1 --num 16
+python simul.py --algo sjf --batch 1 --num 16
 
+python simul.py --algo prema --batch 1 --num 12
+python simul.py --algo prema --batch 1 --num 12
+python simul.py --algo hpf --batch 1 --num 12
+python simul.py --algo hpf --batch 1 --num 12
+python simul.py --algo token --batch 1 --num 12
+python simul.py --algo token --batch 1 --num 12
+python simul.py --algo sjf --batch 1 --num 12
+python simul.py --algo sjf --batch 1 --num 12
+
+python simul.py --algo prema --batch 1 --num 16
+python simul.py --algo prema --batch 1 --num 16
+python simul.py --algo hpf --batch 1 --num 16
+python simul.py --algo hpf --batch 1 --num 16
+python simul.py --algo token --batch 1 --num 16
+python simul.py --algo token --batch 1 --num 16
+python simul.py --algo sjf --batch 1 --num 16
+python simul.py --algo sjf --batch 1 --num 16
+
+python simul.py --algo prema --batch 1 --num 12
+python simul.py --algo prema --batch 1 --num 12
+python simul.py --algo hpf --batch 1 --num 12
+python simul.py --algo hpf --batch 1 --num 12
+python simul.py --algo token --batch 1 --num 12
+python simul.py --algo token --batch 1 --num 12
+python simul.py --algo sjf --batch 1 --num 12
+python simul.py --algo sjf --batch 1 --num 12
+
+python simul.py --algo prema --batch 4 --num 16
+python simul.py --algo prema --batch 4 --num 16
+python simul.py --algo hpf --batch 4 --num 16
+python simul.py --algo hpf --batch 4 --num 16
+python simul.py --algo token --batch 4 --num 16
+python simul.py --algo token --batch 4 --num 16
+python simul.py --algo sjf --batch 4 --num 16
+python simul.py --algo sjf --batch 4 --num 16
+
+python simul.py --algo prema --batch 4 --num 12
+python simul.py --algo prema --batch 4 --num 12
+python simul.py --algo hpf --batch 4 --num 12
+python simul.py --algo hpf --batch 4 --num 12
+python simul.py --algo token --batch 4 --num 12
+python simul.py --algo token --batch 4 --num 12
+python simul.py --algo sjf --batch 4 --num 12
+python simul.py --algo sjf --batch 4 --num 12
+
+python simul.py --algo prema --batch 4 --num 16
+python simul.py --algo prema --batch 4 --num 16
+python simul.py --algo hpf --batch 4 --num 16
+python simul.py --algo hpf --batch 4 --num 16
+python simul.py --algo token --batch 4 --num 16
+python simul.py --algo token --batch 4 --num 16
+python simul.py --algo sjf --batch 4 --num 16
+python simul.py --algo sjf --batch 4 --num 16
+
+python simul.py --algo prema --batch 4 --num 12
+python simul.py --algo prema --batch 4 --num 12
+python simul.py --algo hpf --batch 4 --num 12
+python simul.py --algo hpf --batch 4 --num 12
+python simul.py --algo token --batch 4 --num 12
+python simul.py --algo token --batch 4 --num 12
+python simul.py --algo sjf --batch 4 --num 12
+python simul.py --algo sjf --batch 4 --num 12
